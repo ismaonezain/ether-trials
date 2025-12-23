1829311718937378e5d6420151cb5226e2195a38
ismaonezain <88978640+ismaonezain@users.noreply.github.com>
2025-12-24 05:27:02 +0700

    Create WalletStatus.tsx

---
+++ src/components/game/WalletStatus.tsx
@@ -0,0 +1,8 @@
+'use client';
+
+import { useAccount, useConnectors } from 'wagmi';
+
+export function WalletStatus(): JSX.Element {
+  // Status hidden - only show Farcaster profile instead
+  return <></>;
+}
