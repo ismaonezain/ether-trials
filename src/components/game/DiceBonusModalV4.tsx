2c247e85d413816bd680561cb0b3f10ed4275f10
ismaonezain <88978640+ismaonezain@users.noreply.github.com>
2026-01-31 06:19:34 +0700

    Update DiceBonusModalV4.tsx

--- src/components/game/DiceBonusModalV4.tsx
+++ src/components/game/DiceBonusModalV4.tsx
@@ -1,4 +1,5 @@
-'use client';
+bantu aku dong
+  'use client';
 
 import { useState, useEffect } from 'react';
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
